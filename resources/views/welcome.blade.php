87a6b682c9a2bd5024cdb91bb4c244b6cefd1e61
HUP234 Design <37693783+hup234design@users.noreply.github.com>
2024-01-23 00:50:44 +0000

    typo

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,7 +6,7 @@
     <title>{{ config('app.name', 'Laravel') }}</title>
     <script src="https://cdn.tailwindcss.com"></script>
 </head>
-<body class="antialiased bg-[##1E2C47]">
+<body class="antialiased bg-[#1E2C47]">
 <div class="h-screen w-full flex flex-col items-center justify-center">
     <img src="https://calibratree.co.uk/images/logos/calibra-tree-surgeons-logo-white.png" class="w-[640px] max-w-3/4">
 </div>
